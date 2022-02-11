a410e4595a50991df14768ca483e5e67bd411ac8
JoshGenereux <josh.genereux13@gmail.com>
2022-02-11 09:06:14 -0700

    redux set up

---
+++ src/redux/budgetReducer.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+  purchases: [],
+  budgetLimit: null,
+  loading: false
+}
+
+const reducer = (state, action)=>{
+  return state = initialState;
+}
+
+export default reducer;
