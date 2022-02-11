b8ffe09b118843eab8fb5d118737c3ab68ddc5d5
JoshGenereux <josh.genereux13@gmail.com>
2022-02-11 10:03:26 -0700

    finished

--- src/redux/budgetReducer.js
+++ src/redux/budgetReducer.js
@@ -7,6 +7,8 @@ const initialState = {
 }
 
 const REQUEST_BUDGET_DATA = 'REQUEST_BUDGET_DATA';
+const ADD_PURCHASE = 'ADD_PURCHASE';
+const REMOVE_PURCHASE = 'REMOVE_PURCHASE';
 
 export function requestBudgetData(){
   let data = axios.get('/api/budget-data')
@@ -17,12 +19,39 @@ export function requestBudgetData(){
   }
 }
 
+export function addPurchase(price, description, category){
+  let data = axios.get('/api/budget-data/purchase', {
+    description, price, category
+  }).then(res => res.data)
+  return {
+    type: ADD_PURCHASE,
+    payload: data
+  }
+}
+
+export function removePurchase(id){
+  let data = axios.delete(`/api/budget-data/purchase/:${id}`)
+    .then(res => res.data)
+  return {
+    type: REMOVE_PURCHASE,
+    payload: data
+  }
+}
+
 const reducer = (state=initialState, action)=>{
   switch (action.type){
     case REQUEST_BUDGET_DATA + '_PENDING':
       return {...state, loading: true}
     case REQUEST_BUDGET_DATA + '_FULFILLED':
       return {...state, ...action.payload, loading: false}
+    case ADD_PURCHASE + '_PENDING':
+      return { ...state, loading: true }
+    case ADD_PURCHASE + '_FULFILLED':
+      return { ...state, purchases: action.payload, loading: false }
+    case REMOVE_PURCHASE + '_PENDING':
+      return {...state, loading: true}
+    case REMOVE_PURCHASE + '_FULFILLED':
+      return {...state, loading: false, purchases: action.payload}
     default:
       return state;
   }
